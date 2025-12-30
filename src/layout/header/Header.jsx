7859b823fe72647ded6f5a9134db800c906a79c6
apurvaNeo <apurva.joshi@neosoftmail.com>
2025-12-30 19:46:35 +0530

    api integration in review pages

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -34,39 +34,41 @@ const Header = () => {
       //    </div>
       // </nav>
       <nav className="header_wrapper navbar navbar-expand-lg navbar-light container mt-2 mb-4">
-         <div className="d-flex align-items-center justify-content-between w-100">
+         <div className="d-flex align-items-lg-center justify-content-between w-100">
 
          <NavLink className="navbar-brand" to="/">
             <img src={logo} alt="Logo" />
          </NavLink>
 
-         <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-         >
-            <span className="navbar-toggler-icon"></span>
-         </button>
+         <div>
+            <button
+               className="navbar-toggler"
+               type="button"
+               data-bs-toggle="collapse"
+               data-bs-target="#navbarSupportedContent"
+            >
+               <span className="navbar-toggler-icon"></span>
+            </button>
 
-         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-               <li className="nav-item">
-                  <NavLink className="nav-link" to="/" end>Home</NavLink>
-               </li>
-               <li className="nav-item">
-                  <NavLink className="nav-link" to="/work">Our Work</NavLink>
-               </li>
-               <li className="nav-item">
-                  <NavLink className="nav-link" to="/about">About Us</NavLink>
-               </li>
-               <li className="nav-item">
-                  <NavLink className="nav-link" to="/review">Review</NavLink>
-               </li>
-               <li className="nav-item">
-                  <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
-               </li>
-            </ul>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                     <NavLink className="nav-link" to="/" end>Home</NavLink>
+                  </li>
+                  <li className="nav-item">
+                     <NavLink className="nav-link" to="/work">Our Work</NavLink>
+                  </li>
+                  <li className="nav-item">
+                     <NavLink className="nav-link" to="/about">About Us</NavLink>
+                  </li>
+                  <li className="nav-item">
+                     <NavLink className="nav-link" to="/review">Review</NavLink>
+                  </li>
+                  <li className="nav-item">
+                     <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
+                  </li>
+               </ul>
+            </div>
          </div>
 
          </div>
--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,12 +1,59 @@
 import Header from '../layout/header/Header';
-import Footer from '../layout/footer/Footer'
+import Footer from '../layout/footer/Footer';
+import React,{useState,useEffect} from 'react';
+
+
 const ReviewPage = ()=>{
+    const [users,setUser] = useState([]);
+    const [search,setSearch] = useState('');
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUser(data))
+        .catch(error => console.error('Error:', error));
+    },[]);
+
     return(
         <div>
             <div className='Top_section'>
                 <Header/>
             </div>
-            <h2>Review</h2>
+            <div className='container'>
+                <div className='table_wrapper'>
+                    <h5 className='mb-3'>API Integration</h5>
+                    <div className='row d-flex justify-content-end'>
+                        <div className='col-md-5'>
+                            <div className='form-group mb-3'>
+                                <input type="text" placeholder='search by Name' className='form-control' value={search} onChange={(e)=>setSearch(e.target.value)} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='table-responsive'>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Phone</th>
+                                    <th scope="col">Website</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map((user)=>{
+                                    return(
+                                        <tr key={user.id}>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.phone}</td>
+                                            <td>{user.website}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
